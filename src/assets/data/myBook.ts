a64701ec3391a9531814d9ad84d21c23e8e3ed25
nguye374 <nguye374@sheridancollege.ca>
2021-07-29 04:06:50 -0400

    finish

--- src/assets/data/myBook.ts
+++ src/assets/data/myBook.ts
@@ -2,7 +2,7 @@ import { book } from "../../app/bookInfo";
 
 export const mybook: book[] = [
     { bookName: "Sans famille", author: "Hector Mallot", genre: "Novel", yearPublish: "1878", picture: "assets/images/S.jpeg" },
-    { bookName: "The Catcher in the Rye", author: "J. D. Salinger", genre: "Novel", yearPublish: "1951", picture: "assets/images/C.jpg" },
+    { bookName: "Catcher in the Rye", author: "J. D. Salinger", genre: "Novel", yearPublish: "1951", picture: "assets/images/C.jpg" },
     { bookName: "The Great Gatsby", author: "F. Scott Fitzgerald", genre: "Novel", yearPublish: "1925", picture: "assets/images/T.jpg" },
-    { bookName: "To Kill a Mockingbird", author: "Harper Lee", genre: "Novel", yearPublish: "1960", picture: "assets/images/M.jpg" }
+    { bookName: "Kill a Mockingbird", author: "Harper Lee", genre: "Novel", yearPublish: "1960", picture: "assets/images/M.jpg" }
 ]
